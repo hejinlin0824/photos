5b35168ad8a7de46fd12f7d63b442d8719e7a725
何锦琳 <147542324+hejinlin0824@users.noreply.github.com>
2024-09-21 19:31:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-// 图片文件名列表
-const imageFiles = [
-    'img1.jpg',
-    'img2.jpg',
-    'img3.jpg',
-    'img4.jpg',
-    'img5.jpg',
-    'img6.jpg',
-    // 你可以添加更多图片，不需要修改 HTML 文件
-];
-
 // 获取相册容器
 const galleryContainer = document.getElementById('gallery');
 
-// 动态创建相册图片元素
-imageFiles.forEach((imageFile, index) => {
-    const div = document.createElement('div');
-    div.classList.add('gallery-item');
+// 从 images.json 加载图片并动态创建相册图片元素
+fetch('images.json')
+  .then(response => response.json())
+  .then(images => {
+    images.forEach((imageFile, index) => {
+      const div = document.createElement('div');
+      div.classList.add('gallery-item');
 
-    const img = document.createElement('img');
-    img.src = `images/${imageFile}`;
-    img.alt = `Image ${index + 1}`;
-    img.classList.add('gallery-img');
+      const img = document.createElement('img');
+      img.src = `images/${imageFile}`;
+      img.alt = `Image ${index + 1}`;
+      img.classList.add('gallery-img');
 
-    div.appendChild(img);
-    galleryContainer.appendChild(div);
-});
+      div.appendChild(img);
+      galleryContainer.appendChild(div);
+    });
+
+    // 为所有图片添加点击事件
+    setupImageClickEvents();
+  });
 
 // 获取模态窗口元素
 const modal = document.getElementById('modal');
 const modalImg = document.getElementById('modal-img');
 const captionText = document.getElementById('caption');
 
-// 所有图片点击事件
-galleryContainer.addEventListener('click', function (event) {
-    if (event.target.classList.contains('gallery-img')) {
-        modal.style.display = "flex";
-        modalImg.src = event.target.src;
-        captionText.innerHTML = event.target.alt;
-    }
-});
+// 设置图片点击事件
+function setupImageClickEvents() {
+  const galleryImages = document.querySelectorAll('.gallery-img');
+  galleryImages.forEach(img => {
+    img.addEventListener('click', function () {
+      modal.style.display = "flex";
+      modalImg.src = img.src;
+      captionText.innerHTML = img.alt;
+    });
+  });
+}
 
 // 关闭模态窗口
 const close = document.getElementsByClassName("close")[0];
